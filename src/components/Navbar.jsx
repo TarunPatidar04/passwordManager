a820f8766268416c8c18ae0343f0cc0cf2c1f055
TarunPatidar04 <tarun.coderfarm@gmail.com>
2024-04-07 19:01:20 +0530

    Create a Basic Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <nav className="bg-purple-200 flex justify-between items-center px-4 h-14">
+      <div className="logo font-bold">Passop</div>
+      <ul>
+        <li className="flex gap-4">
+          <a className="hover:font-bold cursor-pointer" href="#">Home</a>
+          <a className="hover:font-bold cursor-pointer" href="#">About</a>
+          <a className="hover:font-bold cursor-pointer" href="#">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
